6e1254bd15f7203f4873c43298b926baa0f1e4ab
nithia <nithia341@gmail.com>
2024-12-23 19:18:14 -0600

    modifique cursos

---
+++ src/componentes/estudios/Certificados.jsx
@@ -0,0 +1,39 @@
+
+
+import ibmA from "../../imagenes/ibmA.jpg";
+import IBMC from "../../imagenes/IBMC.jpg";
+import python from "../../imagenes/python.jpg";
+import G7 from "../../imagenes/G7.jpg";
+import javascript from "../../imagenes/javascript.jpg";
+import "../estudios/estudios.css";
+
+
+const Certificados=[
+  
+{
+  id:1,
+  name:"pyton",
+  image:python,
+},
+{
+  id:2,
+  name:"IBM Conceptos",
+  image:IBMC,
+},
+{
+  id:3,
+  name:"IBM Advanced",
+  image:ibmA,
+},
+{
+  id:4,
+  name:"javascript",
+  image:javascript,
+},
+{
+  id:5,
+  name:"G7",
+  image:G7,
+},
+];
+export default Certificados;
--- src/componentes/estudios/Estudios.jsx
+++ src/componentes/estudios/Estudios.jsx
@@ -1,95 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import CardMedia from '@mui/material/CardMedia';
-import ibmA from "../../imagenes/ibmA.jpg";
-import IBMC from "../../imagenes/IBMC.jpg";
-import python from "../../imagenes/python.jpg";
-import G7 from "../../imagenes/G7.jpg";
-import javascript from "../../imagenes/javascript.jpg";
-import "../estudios/estudios.css";
-import ArrowDropUpSharpIcon from '@mui/icons-material/ArrowDropUpSharp';
+import React from 'react';
+import Slider from 'react-slick'; // Importar el componente Slider de react-slick
+import '../estudios/estudios.css'; // Estilos personalizados
+import 'slick-carousel/slick/slick.css'; // Estilos base de Slick
+import 'slick-carousel/slick/slick-theme.css'; // Estilos de tema de Slick
+import Certificados from './Certificados'; // Importación de los certificados
 
-export default function Estudios() {
-  const [isVisible, setIsVisible] = useState(false);
+// Configuración del Slider
+const settings = {
+  infinite: true,
+  speed: 20,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  arrows: true,
+};
 
-  useEffect(() => {
-    // Escuchar el evento de scroll
-    const handleScroll = () => {
-      if (window.scrollY > 300) {  // Mostrar el botón después de 300px de desplazamiento
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    };
-
-    // Agregar el event listener para el scroll
-    window.addEventListener('scroll', handleScroll);
-
-    // Eliminar el event listener cuando el componente se desmonte
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  // Función para hacer scroll hacia arriba
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',  // Desplazamiento suave
-    });
-  };
-
+const Estudios = () => {
   return (
     <div className="i">
-      <div className="titulo">
-        <h3>Cursos</h3>
-      </div>
-
-      <div className="tarjetas">
-        <div className="card">
-          <CardMedia
-            className="img"
-            alt="Python"
-            image={python}
-          />
-        </div>
-        <div className="card">
-          <CardMedia
-            className="img"
-            alt="IBM A"
-            image={IBMC}
-          />
-        </div>
-        <div className="card">
-          <CardMedia
-            className="img"
-            alt="IBM A"
-            image={ibmA}
-          />
-        </div>
-        <div className="card">
-          <CardMedia
-            className="img"
-            alt="JavaScript"
-            image={javascript}
-          />
-        </div>
-
-        <div className="card">
-          <CardMedia
-            className="img"
-            alt="G7"
-            image={G7}
-          />
-        </div>
-      </div>
-
-      {/* Botón para subir hacia arriba */}
-      {isVisible && (
-        <button 
-          onClick={scrollToTop}  // Llamamos a la función scrollToTop al hacer clic
-          className="scroll-to-top-button"
-        >
-          <ArrowDropUpSharpIcon />
-        </button>
-      )}
+      <div className="titulo">Certificados</div>
+      <Slider {...settings}>
+        {Certificados.map(({ id, name, image }) => (
+          <div key={id} className="certificado">
+            <img src={image} alt={name} className="certificado-imagen" />
+            <h3>{name}</h3>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
-}
+};
+
+export default Estudios;
